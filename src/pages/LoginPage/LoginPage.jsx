188e7ae80e5fa9a311185093b1e29e734e8a2655
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-12 11:22:30 +0800

    debug

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
