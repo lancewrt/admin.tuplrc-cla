4f3bc4057246cc1a730c085a084a203b22c1f638
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-12 11:22:02 +0800

    debug

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_URL } from '../config';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const LoginPage = () => {
