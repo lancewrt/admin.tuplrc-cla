6b3238d1f879408f409248cf1c2fea6a1d0b3db5
Pauleen Dingcong <pauleenanndingcong@gmail.com>
2025-04-12 09:25:15 +0800

    remove sw

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import Swal from 'sweetalert2';
 import {Provider} from 'react-redux'
 import store from './store/store.js';
-import { register } from './serviceWorkerRegistration'; // Import the register function
+
 
 // Define global SweetAlert2
 window.Swal = Swal;
@@ -35,4 +35,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-register();
