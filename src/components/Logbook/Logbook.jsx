dd6a53067b84dfb878f056946377f3d773cc5b84
lancewrt <91044173+lancewrt@users.noreply.github.com>
2025-04-21 11:14:19 +0800

    update

--- src/components/Logbook/Logbook.jsx
+++ src/components/Logbook/Logbook.jsx
@@ -29,7 +29,7 @@ const Logbook = () => {
             // Clean up event listener
             return () => {
                 socket.off('attendanceUpdated');
-                socket.disconnect();
+               
                 
             };
         }
